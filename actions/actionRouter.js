54705e002358f2f55cc2cbf92ad980aa9f7c56f8
MicahJank <MicahJank@users.noreply.github.com>
2019-11-10 20:25:33 -0600

    added basic boilerplate to the projectRouter and ActionRouter

---
+++ actions/actionRouter.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const Action = require('../data/helpers/actionModel.js');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+
+});
+
+
+module.exports = router;
---
+++ projects/projectRouter.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const Project = require('../data/helpers/projectModel.js');
+const Action = require('../data/helpers/actionModel.js');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+
+});
+
+
+module.exports = router;

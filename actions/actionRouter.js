0a9fe17a60ac7e022e3f27e0556c7002521307bf
MicahJank <MicahJank@users.noreply.github.com>
2019-11-10 22:08:12 -0600

    MVP almost completed only need to figure out the issue with the action PUT request

--- actions/actionRouter.js
+++ actions/actionRouter.js
@@ -18,6 +18,34 @@ router.get('/:id', validateID, (req, res) => {
 });
 
 
+router.put('/:id', validateID, validateBody, (req, res) => {
+    const { id } = req.params;
+    Action.update(id, req.body)
+        .then(action => {
+            res.status(200).json(action);
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'There was a problem updating the action in the database'
+            });
+        });
+});
+
+router.delete('/:id', validateID, (req, res) => {
+    const { id } = req.params;
+    Action.remove(id)
+        .then(num => {
+            if(num > 0) {
+                res.status(200).json(req.action);
+            } else {
+                res.status(404).json({ message: 'Could not find action with the specified id'} );
+            };
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'There was a problem deleting the action from the database' });
+        });
+})
+
 
 function validateID(req, res, next) {
     const { id } = req.params;
@@ -39,5 +67,18 @@ function validateID(req, res, next) {
             });
         });
 }
+
+function validateBody(req, res, next) {
+    const { project_id, description, notes } = req.body;
+
+    const { project_id, description, notes } = req.body;
+    if(Object.entries(req.body).length === 0) {
+        res.status(400).json({ message: 'No body data was found' });
+    } else if(!project_id || !description || !notes) {
+        res.status(400).json({ message: `Body is missing data. Please check that you have a 'project_id' field, 'description' field and 'notes' field inside the body request. ` });
+    } else {
+        next();
+    }
+};
 
 module.exports = router;

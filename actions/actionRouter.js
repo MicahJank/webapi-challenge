8639824c32ed2491c535dd31bb34099f6b27215c
MicahJank <MicahJank@users.noreply.github.com>
2019-11-10 21:41:58 -0600

    used a get and validateID middleware to get the specific action from the database

--- actions/actionRouter.js
+++ actions/actionRouter.js
@@ -4,8 +4,40 @@ const Action = require('../data/helpers/actionModel.js');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-
+    Action.get()
+        .then(actions => {
+            res.status(200).json(actions);
+        })
+        .catch(err => {
+            res.status(500).json( { message: 'Could not retrieve the list of actions from the database' });
+        });
 });
 
+router.get('/:id', validateID, (req, res) => {
+    res.status(200).json(req.action);
+});
+
+
+
+function validateID(req, res, next) {
+    const { id } = req.params;
+    
+    Action.get(id)
+        .then(action => {
+            if(action) {
+                req.action = action;
+                next();
+            } else {
+                res.status(404).json({
+                    message: 'Couldnt find the id of the action, please make sure the id is valid'
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'There was a problem getting the action from the database'
+            });
+        });
+}
 
 module.exports = router;
